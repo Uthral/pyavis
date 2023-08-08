50896ba23a71409eabd58fe86c0d2098bc3272ab
Uthral <robin.kuelker@outlook.de>
2023-08-08 15:49:30 +0200

    Enable basic drawing in spectrogram

--- pyavis/backends/qt/spectrogram/spectrogram.py
+++ pyavis/backends/qt/spectrogram/spectrogram.py
@@ -60,9 +60,21 @@ class SpectogramQt(AbstractSpectrogramVisualizer):
         return self.widget
     
     def reset(self):
+        '''
+        Reset the image to the origninal :class:`Astft <pya.Astft>`
+        '''
         self.img.setImage(np.abs(self.orig_spectrogram.stft.T))
 
-    def return_as_asig(self) -> Asig:
+    def return_as_asig(self, **kwargs) -> Asig:
+        '''
+        Return the displayed STFT as an :class:`Asig <pya.Asig>`.
+
+        Parameters
+        ----------
+        **kwargs :
+            Keyword arguments for :func:`Astft.to_sig()`
+        '''
+
         # Image is transposed spectrum -> Transpose again
         # Image is absolute value of STFT (magnitude)
         # Potential solutions:
@@ -76,6 +88,10 @@ class SpectogramQt(AbstractSpectrogramVisualizer):
         return stft.to_sig()
 
     def return_as_astft(self) -> Astft:
+        '''
+        Return the displayed STFT as an :class:`Astft <pya.Astft>`
+        '''
+
         # Image is transposed spectrum -> Transpose again
         # Image is absolute value of STFT (magnitude)
         # Potential solutions:
@@ -89,24 +105,53 @@ class SpectogramQt(AbstractSpectrogramVisualizer):
         return stft
 
     def draw(self, freq: float, time: float):
+        '''
+        Draw at the time and position with the set brush.
+        Does nothing if no brush data is set.
+
+        Parameters
+        ----------
+        freq: float
+            Frequencey to draw at
+        time: float
+            Time to draw at
+        '''
         adj_freq = freq / ((self.orig_spectrogram.sr / 2) / len(self.orig_spectrogram.freqs))
         adj_time = (time / ((self.orig_spectrogram.samples / self.orig_spectrogram.sr))) * len(self.orig_spectrogram.times)
         pos = QtCore.QPointF(adj_time, adj_freq)
 
-        self.img.setDrawKernel(kernel=np.ones((50,2)), mask=np.ones((50,2)), center=(5,1), mode='set')
-        self.img.drawAt(pos)
-        self.img.setDrawKernel()
+        # Enable draw mode for the operation, disable afterwards.
+        if self.brush_data is not None:
+            self.draw_mode(True)
+            self.img.drawAt(pos)
+            self.draw_mode(False)
 
-    def set_draw_shape(self, data):
-        # self.brush_data = ...
-        # self.brush_mask = ...
-        # self.draw_mode = "add" or "set" or func
-        pass
+    def set_brush_data(self, brush_data=None, brush_mask=None, brush_center=(0,0), draw_mode="add"):
+        """ 
+        Set the brush shape, values, center and mode.
+        See :function:`drawAt` from :class:`ImageItem <pyqtgraph.ImageItem>` for more details.
 
-    def draw_mode(self, active: bool = False):
-        # if active:
-        #   self.img.setDrawKernel(...)
-        # else:
-        #   self.img.setDrawKernel()
-        pass
+        Parameters
+        ----------
+        brush_data : np.ndarray, default: None
+            Values that should be used for drawing
+        brush_mask : np.ndarray, optional, default: None
+            Mask with values between 0-1. Effect: image * (1-mask) + data * mask
+        brush_center : tuple, default: (0, 0)
+            Center of the brush
+        draw_mode : str, optional, default: "add"
+            Either "add" or "set". If "set", then mask will be ignored.
+        """
+        self.brush_data = brush_data
+        self.brush_mask = brush_mask
+        self.brush_center = brush_center
+        self.brush_mode = draw_mode
         
+    def draw_mode(self, active: bool = False):
+        """ 
+        Enable or disable draw mode.
+        """
+        if active:
+           self.img.setDrawKernel(self.brush_data, self.brush_mask, self.brush_center, self.brush_mode)
+        else:
+           self.img.setDrawKernel()
